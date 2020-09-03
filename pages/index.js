ae99b0b0f3d094123c9f421dd276ca17621ae260
Dustin Morris <dustinnuc@gmail.com>
2020-09-02 17:38:15 -0700

    Added fonts and formatted background colors.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <>
+    <div className={styles.background}>
+    <div className={styles.left}></div>
+    <div className={styles.right}></div>
+    </div>
     <div className={styles.container}>
       <Head>
         <title>Dustin W Morris - Software/Systems Engineer</title>
@@ -10,14 +15,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <img className={styles.monogram}src="/images/monogram.svg"/>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Dustin W. Morris
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h2 className={styles.occupation}>
+          Software/Systems Engineer
+        </h2>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -61,5 +65,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
+    </>
   )
 }
